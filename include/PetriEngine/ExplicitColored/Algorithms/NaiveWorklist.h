1e8a015fa33865a4bbe5e9dc5ca48beb86f2f04e
Emil <emilnormannbrandt@gmail.com>
2025-02-07 13:11:21 +0100

    Removal of some unneccesary things + styling

--- include/PetriEngine/ExplicitColored/Algorithms/NaiveWorklist.h
+++ include/PetriEngine/ExplicitColored/Algorithms/NaiveWorklist.h
@@ -1,70 +1,68 @@
 #ifndef NAIVEWORKLIST_H
 #define NAIVEWORKLIST_H
 
-
 #include "PetriEngine/ExplicitColored/ColoredPetriNet.h"
 #include "PetriEngine/ExplicitColored/ColoredResultPrinter.h"
 #include "PetriEngine/ExplicitColored/SearchStatistics.h"
 
-namespace PetriEngine {
-    namespace ExplicitColored {
+namespace PetriEngine::ExplicitColored {
+    template <typename T>
+    class RDFSStructure;
+    class ColoredResultPrinter;
+
+    enum class Quantifier {
+        EF,
+        AG
+    };
+
+    enum class SearchStrategy {
+        DFS,
+        BFS,
+        RDFS,
+        HEUR,
+        EDFS,
+        EBFS,
+        ERDFS,
+        EHEUR
+    };
+
+    class NaiveWorklist {
+    public:
+        NaiveWorklist(
+            const ColoredPetriNet& net,
+            const PQL::Condition_ptr &query,
+            const std::unordered_map<std::string, uint32_t>& placeNameIndices,
+            const std::unordered_map<std::string, Transition_t>& transitionNameIndices,
+            const IColoredResultPrinter& coloredResultPrinter
+        );
+
+        bool check(SearchStrategy searchStrategy, size_t seed);
+        const SearchStatistics& GetSearchStatistics() const;
+    private:
+        PQL::Condition_ptr _gammaQuery;
+        Quantifier _quantifier;
+        const ColoredPetriNet& _net;
+        const std::unordered_map<std::string, uint32_t>& _placeNameIndices;
+        const std::unordered_map<std::string, Transition_t> _transitionNameIndices;
+
+        bool _check(const ColoredPetriNetMarking& state) const;
+
         template <typename T>
-        class RDFSStructure;
-        class ColoredResultPrinter;
+        bool _dfs();
+        template <typename T>
+        bool _bfs();
+        template <typename T>
+        bool _rdfs(size_t seed);
+        template <typename T>
+        bool _bestfs(size_t seed);
 
-        enum class Quantifier {
-            EF,
-            AG
-        };
+        template <template <typename> typename WaitingList, typename T>
+        bool _genericSearch(WaitingList<T> waiting);
+        bool _getResult(bool found) const;
 
-        enum class SearchStrategy {
-            DFS,
-            BFS,
-            RDFS,
-            HEUR,
-            EDFS,
-            EBFS,
-            ERDFS,
-            EHEUR
-        };
-
-        class NaiveWorklist {
-        public:
-            NaiveWorklist(
-                const ColoredPetriNet& net,
-                const PQL::Condition_ptr &query,
-                const std::unordered_map<std::string, uint32_t>& placeNameIndices,
-                const std::unordered_map<std::string, Transition_t>& transitionNameIndices,
-                const IColoredResultPrinter& coloredResultPrinter
-            );
-
-            bool check(SearchStrategy searchStrategy, size_t seed);
-            const SearchStatistics& GetSearchStatistics() const;
-        private:
-            PQL::Condition_ptr _gammaQuery;
-            Quantifier _quantifier;
-            const ColoredPetriNet& _net;
-            const std::unordered_map<std::string, uint32_t>& _placeNameIndices;
-            const std::unordered_map<std::string, Transition_t> _transitionNameIndices;
-
-            bool _check(const ColoredPetriNetMarking& state);
-
-            template <typename T>
-            bool _dfs();
-            template <typename T>
-            bool _bfs();
-            template <typename T>
-            bool _rdfs(size_t seed);
-            template <typename T>
-            bool _bestfs(size_t seed);
-
-            template <template <typename> typename WaitingList, typename T>
-            bool _genericSearch(WaitingList<T> waiting);
-            bool _getResult(bool found) const;
-
-            SearchStatistics _searchStatistics;
-            const IColoredResultPrinter& _coloredResultPrinter;
-        };
-    }
+        SearchStatistics _searchStatistics;
+        const IColoredResultPrinter& _coloredResultPrinter;
+    };
 }
+
 #endif //NAIVEWORKLIST_H
